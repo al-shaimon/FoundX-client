68cc7139403c89c6cd9819234cfdac6b87fa092c
al-shaimon <alshaimon968@gmail.com>
2024-09-27 05:18:49 +0600

    part-6

--- src/app/(withCommonLayout)/found-items/[itemId]/page.tsx
+++ src/app/(withCommonLayout)/found-items/[itemId]/page.tsx
@@ -1,18 +1,25 @@
 import Container from '@/src/components/UI/Container';
 import Post from '@/src/components/UI/Post';
 import axiosInstance from '@/src/lib/AxiosInstance';
+import { getPost } from '@/src/services/post';
 import { IPost } from '@/src/types';
 
-const FoundItems = async () => {
-  const { data } = await axiosInstance.get(`/items`);
+interface IProps {
+  params: {
+    itemId: string;
+  };
+}
+
+const ItemDetailPage = async ({ params: { itemId } }: IProps) => {
+  const { data: post } = await getPost(itemId);
 
   return (
     <Container>
       <div className="mx-auto my-3 max-w-[720px]">
-        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
+        <Post key={post?._id} post={post} />
       </div>
     </Container>
   );
 };
 
-export default FoundItems;
+export default ItemDetailPage;
